fd26118ae27f035d8428a5683ea0a6d5817d61fa
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-07-05 01:26:29 -0300

    Agregado bloque que guarda items

--- src/main/java/nachiten/mari/mod/baseClass/MariMod.java
+++ src/main/java/nachiten/mari/mod/baseClass/MariMod.java
@@ -3,18 +3,22 @@ package nachiten.mari.mod.baseClass;
 import nachiten.mari.mod.blockItems.*;
 import nachiten.mari.mod.blocks.*;
 import nachiten.mari.mod.foods.*;
+import nachiten.mari.mod.gui.ExampleBlock;
+import nachiten.mari.mod.gui.ExampleBlockEntity;
 import nachiten.mari.mod.items.*;
+import nachiten.mari.mod.mariCrop.MariCropBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.item.*;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -28,19 +32,44 @@ public class MariMod implements ModInitializer {
 	public static final Item MARIHUANA = new Marihuana(new Item.Settings().group(MariMod.ITEM_GROUP).maxCount(32));
 	public static final Item COCAINA = new Cocaina(new Item.Settings().group(MariMod.ITEM_GROUP).maxCount(32).food(CocainaFood.COCAINA_COMIDA));
 	public static final Item PORRO = new Porro(new Item.Settings().group(MariMod.ITEM_GROUP).maxCount(8).food(PorroFood.PORRO_COMIDA));
-	public static final Item MARIHUANA_SEEDS = new Item(new Item.Settings().group(MariMod.ITEM_GROUP));
 
+	public static final CropBlock MARIHUANA_CROP_BLOCK = new MariCropBlock(AbstractBlock.Settings.of(Material.PLANT).nonOpaque().noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP));
+	public static final Item MARIHUANA_SEEDS = new AliasedBlockItem(MariMod.MARIHUANA_CROP_BLOCK, new Item.Settings().group(MariMod.ITEM_GROUP));
+
+	// ---- BLOQUE CON FUNCIONALIDAD ----
+
+	public static final Block EXAMPLE_BLOCK = new ExampleBlock(FabricBlockSettings.of(Material.METAL));
+
+	public static BlockEntityType<ExampleBlockEntity> EXAMPLE_BLOCK_ENTITY;
+
+	static final String modName = "marimod";
 
 	@Override
 	public void onInitialize() {
 		registrarObjetos();
+
+		registrarSemillasMarihuana();
+
+		registrarBloqueConFuncionalidad();
+	}
+
+	void registrarBloqueConFuncionalidad() {
+		Registry.register(Registry.BLOCK, new Identifier(modName, "gui_block"), EXAMPLE_BLOCK);
+		EXAMPLE_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, "marimod:demo_block_entity", FabricBlockEntityTypeBuilder.create(ExampleBlockEntity::new, EXAMPLE_BLOCK).build(null));
 	}
 
 	void registrarObjetos() {
-		Registry.register(Registry.BLOCK, new Identifier("marimod", "marihuana_block"), MARIHUANA_BLOCK);
-		Registry.register(Registry.ITEM, new Identifier("marimod", "marihuana_block_item"), MARIHUANA_BLOCK_ITEM);
-		Registry.register(Registry.ITEM, new Identifier("marimod", "marihuana"), MARIHUANA);
-		Registry.register(Registry.ITEM, new Identifier("marimod", "cocaina"), COCAINA);
-		Registry.register(Registry.ITEM, new Identifier("marimod", "porro"), PORRO);
+		Registry.register(Registry.BLOCK, new Identifier(modName, "marihuana_block"), MARIHUANA_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(modName, "marihuana_block_item"), MARIHUANA_BLOCK_ITEM);
+		Registry.register(Registry.ITEM, new Identifier(modName, "marihuana"), MARIHUANA);
+		Registry.register(Registry.ITEM, new Identifier(modName, "cocaina"), COCAINA);
+		Registry.register(Registry.ITEM, new Identifier(modName, "porro"), PORRO);
+	}
+
+	void registrarSemillasMarihuana(){
+		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), MARIHUANA_CROP_BLOCK);
+
+		Registry.register(Registry.BLOCK, new Identifier(modName,"marihuana_crop_block"), MARIHUANA_CROP_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(modName,"marihuana_seeds"), MARIHUANA_SEEDS);
 	}
 }
